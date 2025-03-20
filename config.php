f43f0a7afc72d8ba9e15743978ce1235ed9a2fa6
candemirhan <68512243+candemirhan@users.noreply.github.com>
2025-03-20 15:31:22 +0300

    Add config.php 1742473881835

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// Veritabanı Bağlantı Bilgileri
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'cloud_storage');
+
+// FTP Sunucu Bilgileri
+define('FTP_HOST', '');
+define('FTP_USER', '');
+define('FTP_PASS', '');
+define('FTP_PORT', 21);
+
+// Site Ayarları
+define('SITE_TITLE', 'Bulut Depolama Sistemi');
+define('SITE_LOGO', 'assets/img/logo.png');
+define('SITE_FAVICON', 'assets/img/favicon.ico');
+
+// Oturum Güvenliği
+define('SESSION_PREFIX', 'cloud_');
+?>
